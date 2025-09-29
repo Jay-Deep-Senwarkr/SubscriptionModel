1ac9e55107c3c853419a3bc6625be9c66eff1b45
Jaydeep <jdsenwarkr@gmail.com>
2025-09-29 14:04:00 +0530

    Authentication Complete

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -41,6 +41,7 @@ export const signUp = async (req, res, next) => {
         next(error)
     }
 };
+
 export const signIn = async (req, res, next) => {
     try {
         const {email, password} = req.body
@@ -52,6 +53,25 @@ export const signIn = async (req, res, next) => {
             error.statusCode = 404
             throw error
         }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid){
+            const error = new Error('Invalid password')
+            error.statusCode = 401
+            throw error
+        }
+
+        const token = jwt.sign({userId: user._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN})
+
+        res.status(200).json({
+            success: true,
+            message: 'User signed in successfully',
+            data: {
+                token,
+                user,
+            }
+        })
     } catch (error){
         next(error)
     }
